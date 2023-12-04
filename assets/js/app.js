40126a36121770d4ce45d5845888cce06e7ed9df
kwamea236 <kwamea236@protonmail.com>
2023-12-04 05:04:39 +0000

    Update skills page updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,8 +8,8 @@ var sticky = navbar.offsetTop;
 
 function myFunction(){
     if (window.scrollY > sticky){
-        navbar.classList.add("fixed-top")
+        navbar.classList.add("fixed-top","fade-in-item");
     }else{
-        navbar.classList.remove("fixed-top")
+        navbar.classList.remove("fixed-top");
     }
 }
